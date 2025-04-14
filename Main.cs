2aae42cd4c152b06e7eaeccaa9831ad98281a3d0
OpDragon26 <25opdragon25@gmail.com>
2025-04-14 20:43:14 +0200

    bitboard move lookup

--- Main.cs
+++ Main.cs
@@ -7,36 +7,35 @@ Match match = new Match(new Board(Presets.StartingBoard), Type.Autoplay, depth: 
 //Match match = new Match(new Board("3r1k2/8/8/8/8/8/8/3RK3 w - - 0 1"), Type.Analysis, depth: 6, debug: false, moves: 400);
 //match.Print(0);
 
-
 //match.SpeedTest();
-match.Play();
+//match.Play();
 
 //Search.SearchBoard(match.board);
 
 //Move[] moves = Search.SearchBoard(match.board);
 
 /*
-Console.WriteLine("Rook captures");
+Console.WriteLine("Rook bitboards");
 Console.WriteLine("{");
 for (int i = 0; i < 8; i++) // for every row in the array
 {
     Console.Write("{");
     for (int j = 0; j < 8; j++) // for every item in row
     {
-        Console.Write(Bitboards.MagicLookup.RookCapture[i, j] + ",");
+        Console.Write(Bitboards.MagicLookup.RookBitboardNumbers[i, j] + ",");
     }
     Console.Write("},\n");
 }
 Console.WriteLine("}");
 
-Console.WriteLine("Bishop captures");
+Console.WriteLine("Bishop bitboards");
 Console.WriteLine("{");
 for (int i = 0; i < 8; i++) // for every row in the array
 {
     Console.Write("{");
     for (int j = 0; j < 8; j++) // for every item in row
     {
-        Console.Write(Bitboards.MagicLookup.BishopCapture[i, j] + ",");
+        Console.Write(Bitboards.MagicLookup.BishopBitboardNumbers[i, j] + ",");
     }
     Console.Write("},\n");
 }
