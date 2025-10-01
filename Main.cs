239e8c71c13bef33d152eaf5525b0c82a539ea30
OpDragon26 <25opdragon25@gmail.com>
2025-10-01 17:25:35 +0200

    idc at this point

--- Main.cs
+++ Main.cs
@@ -3,32 +3,15 @@ using Type = Blaze.Type;
 
 Bitboards.Init();
 
-//Hasher.Init();
-//Book.Init(Books.Test);
+Hasher.Init();
+Book.Init(Books.Test);
 
-//new Match(new Board("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 0"), Type.Analysis, side: 0, depth: 6, debug: false, dynamicDepth: false).Play();
+new Match(new Board(Presets.StartingBoard), Type.Autoplay, side: 0, depth: 6, debug: false, dynamicDepth: true).Play();
 
 //Search.SearchBoard(new(Presets.StartingBoard));
 //new Match(new Board("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1"), Type.Analysis).Play();
 
-Perft.Run(5, "kiwipete");
-
-
-Board kiwipete = new("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 0");
-//kiwipete.MakeMove(new Move("a1b1", kiwipete));
-//Board plusOne = new("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/1R2K2R b Kkq - 1 1");
-
-Perft.Breakdown(new Board(kiwipete), 3);
-//Perft.Breakdown(new Board(plusOne), 2);
-
-
-//plusOne.CompareTo(kiwipete);
-
-
-//Parser.PrintGame(Parser.ParseUCI("e2e4 d7d5 e4d5 g8f6 b1c3 f6d5 c3d5 d8d5 d2d4 b8c6 g1f3 c8g4 f1e2 e8c8\n"),0);
-//Perft.AnalyzeBoard(new("8/8/8/1KPpP1r1/7k/8/8/8 w - d6 0 3"));
-//match.Print(0);
-
+//Perft.Run(5, "kiwipete");
 
 /*
 Console.WriteLine("Block move lookup");
